f696c80cf378d35637095d28a66735f0df81ee12
G <veraale144@gmail.com>
2026-05-05 16:31:41 -0300

    Update routing to use '/' instead of '/api'

--- app.mjs
+++ app.mjs
@@ -17,7 +17,6 @@ app.set('views', path.resolve('./views'))
 app.set('layout','layout')
 app.use(expressEjsLayouts);
 
-//
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.set('views', path.join(__dirname, 'src', 'views'));
@@ -34,7 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 mongoDbCon();
 
 //Router
-app.use('/api', router)
+app.use('/', router)
 
 //Para las rutas no encontradas
 app.use((req,res)=>{
